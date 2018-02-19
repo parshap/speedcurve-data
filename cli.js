76c468778fa9700b78d75c1c5488180418ef7e50
Parsha Pourkhomami <parshap+git@gmail.com>
2018-02-19 01:04:23 -0800

    Save ndjson files to data directory

--- cli.js
+++ cli.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
+const process = require("process");
+const fs = require("fs");
+const ndjson = require("ndjson");
+const minimist = require("minimist");
 const fetchData = require("./index");
+
+const writeNdJson = (array, path) => {
+  const stream = ndjson.serialize();
+  stream.pipe(fs.createWriteStream(path));
+  array.forEach(data => stream.write(data));
+  stream.end();
+};
 
 const main = async () => {
   const { sites, urls, tests } = await fetchData();
-  console.log(
-    JSON.stringify(
-      {
-        sites,
-        urls,
-        tests,
-      },
-      null,
-      2,
-    ),
-  );
+  const options = minimist(process.argv.slice(2), {
+    default: {
+      data: process.cwd(),
+    },
+  });
+  writeNdJson(sites, `${options.data}/sites.ndjson`);
+  writeNdJson(urls, `${options.data}/urls.ndjson`);
+  writeNdJson(tests, `${options.data}/tests.ndjson`);
 };
 
 main();
